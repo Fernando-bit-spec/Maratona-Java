27685c690fdcb81e062a04eda913b8f6803d0785
Fernando Jesus <fdsantosj15@gmail.com@gmail.com>
2026-02-18 01:01:42 -0300

    introdução a arrays multidimencionais

--- src/estudo/dev/maratonajava/introducao/Aula08ArraysMultidimencionais01.java
+++ src/estudo/dev/maratonajava/introducao/Aula08ArraysMultidimencionais01.java
@@ -3,11 +3,11 @@ package estudo.dev.maratonajava.introducao;
 public class Aula08ArraysMultidimencionais01 {
     public static void main(String[] args) {
         //1,2,3,4,5 meses
-        //31,28,31,30   dias
+        //31,27,31,30 dias
 
         int[][] dias = new int[3][3];
         dias[0][0] = 31;
-        dias[0][0] = 28;
+        dias[0][0] = 27;
         dias[0][0] = 31;
 
         dias[1][0] = 31;
