74b9ce7b9bddb43f246a2056dc3bb5c0261ba055
Fernando Jesus <fdsantosj15@gmail.com@gmail.com>
2026-02-18 00:49:11 -0300

    Implementa iteração de arrays com foreach

---
+++ src/estudo/dev/maratonajava/introducao/Aula08ArraysMultidimencionais01.java
@@ -0,0 +1,25 @@
+package estudo.dev.maratonajava.introducao;
+
+public class Aula08ArraysMultidimencionais01 {
+    public static void main(String[] args) {
+        //1,2,3,4,5 meses
+        //31,28,31,30   dias
+
+        int[][] dias = new int[3][3];
+        dias[0][0] = 31;
+        dias[0][0] = 28;
+        dias[0][0] = 31;
+
+        dias[1][0] = 31;
+        dias[1][1] = 21;
+        dias[1][2] = 31;
+
+        for (int i = 0; i < dias.length; i++) {
+            for (int j = 0; j < dias[0].length; j++) {
+                System.out.println(dias[i][j]);
+            }
+
+        }
+
+    }
+}
