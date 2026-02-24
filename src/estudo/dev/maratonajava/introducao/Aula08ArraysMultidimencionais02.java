8c8a49013135f93b11cac08d59299aa0625ba072
Fernando Jesus <fdsantosj15@gmail.com@gmail.com>
2026-02-23 23:17:32 -0300

    indentação

--- src/estudo/dev/maratonajava/introducao/Aula08ArraysMultidimencionais02.java
+++ src/estudo/dev/maratonajava/introducao/Aula08ArraysMultidimencionais02.java
@@ -5,18 +5,18 @@ public class Aula08ArraysMultidimencionais02 {
         int[][] arrayInt = new int[3][];
 
         arrayInt[0] = new int[2];
-        arrayInt[1] = new int[] {1, 2, 3};
-        arrayInt[2] = new int[]{1 ,2 ,3, 4, 5, 6};
+        arrayInt[1] = new int[]{1, 2, 3};
+        arrayInt[2] = new int[]{1, 2, 3, 4, 5, 6};
 
-        int[][] arrayInt2 = {{0,0}, {1,2,3}, {1,2,3,4,5,6}};
+        int[][] arrayInt2 = {{0, 0}, {1, 2, 3}, {1, 2, 3, 4, 5, 6}};
 
-        for (int []  arrayBase: arrayInt2) {
+        for (int[] arrayBase : arrayInt2) {
             System.out.println("\n-----");
-            for (int num: arrayBase) {
+            for (int num : arrayBase) {
                 System.out.print(num + " ");
-                
+
             }
-            
+
         }
     }
 }
