b460ccac1ad922f393d53a0d0ee6d76065608595
Fernando Jesus <fdsantosj15@gmail.com@gmail.com>
2026-02-23 22:55:59 -0300

    outra forma de inicialização

--- src/estudo/dev/maratonajava/introducao/Aula08ArraysMultidimencionais02.java
+++ src/estudo/dev/maratonajava/introducao/Aula08ArraysMultidimencionais02.java
@@ -10,7 +10,7 @@ public class Aula08ArraysMultidimencionais02 {
 
         int[][] arrayInt2 = {{0,0}, {1,2,3}, {1,2,3,4,5,6}};
 
-        for (int []  arrayBase: arrayInt) {
+        for (int []  arrayBase: arrayInt2) {
             System.out.println("\n-----");
             for (int num: arrayBase) {
                 System.out.print(num + " ");
