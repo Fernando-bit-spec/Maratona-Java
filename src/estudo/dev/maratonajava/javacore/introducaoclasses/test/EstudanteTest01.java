a1eee7efc1fd0254a666aadbb67eda0c743fb967
Fernando Jesus <fdsantosj15@gmail.com@gmail.com>
2026-02-28 20:39:18 -0300

    introdução a POO

---
+++ src/estudo/dev/maratonajava/javacore/introducaoclasses/test/EstudanteTest01.java
@@ -0,0 +1,17 @@
+package estudo.dev.maratonajava.javacore.introducaoclasses.test;
+
+import estudo.dev.maratonajava.javacore.introducaoclasses.dominio.Estudante;
+
+public class EstudanteTest01 {
+    public static void main(String[] args) {
+        Estudante estudante = new Estudante();
+        estudante.nome = "Fernando";
+        estudante.idade = 18;
+        estudante.sexo = 'm';
+
+        System.out.println(estudante.nome);
+        System.out.println(estudante.idade);
+        System.out.println(estudante.sexo);
+        System.out.println(estudante);
+    }
+}
