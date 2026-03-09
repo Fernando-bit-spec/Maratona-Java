e075b297ea2d0e3c8163269d0f6f586573c57a1a
Fernando Jesus <fdsantosj15@gmail.com@gmail.com>
2026-03-09 19:17:34 -0300

    metodos

---
+++ src/estudo/dev/maratonajava/javacore/Bintroducaometodos/test/calculadoraTest01.java
@@ -0,0 +1,11 @@
+package estudo.dev.maratonajava.javacore.Bintroducaometodos.test;
+
+import estudo.dev.maratonajava.javacore.Bintroducaometodos.dominio.Calculadora;
+
+public class calculadoraTest01 {
+    public static void main(String[] args) {
+        Calculadora calculadora = new Calculadora();
+        calculadora.somaDoisNumeros();
+        System.out.println("Finalizando CalculadoraTest01");
+    }
+}
